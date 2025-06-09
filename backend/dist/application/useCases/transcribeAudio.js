8223c7063eb65bca0cd1d88a49828a1ceea3eea6
Esteban Mutis <smithmutis21@gmail.com>
2025-06-09 13:50:20 -0500

    Remove node_modules and build files from tracking, improve .gitignore

--- backend/dist/application/useCases/transcribeAudio.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const transcribeAudio = (transcriptionService, filePath) => {
-    return transcriptionService.transcribe(filePath);
-};
-exports.default = transcribeAudio;
